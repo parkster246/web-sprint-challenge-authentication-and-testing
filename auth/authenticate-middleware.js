105da9ce155c6b26b873fa05848b09e1fe7e1769
Parker williams <parkster246@gmail.com>
2020-09-18 11:23:12 -0500

    npm issues and node issues

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,21 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require('jsonwebtoken')
+const secret = require('../secrets')
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+
+
+module.exports = (req, res,next) => {
+  const token = req.headers.authorization
+
+  if(token){
+    jwt.verify(token, secret.jwtSecret, (error, decodedToken)=>{
+        if(error){
+            res.status(401).json({you : 'Shall not pass'})
+        } else {
+            req.jwt = decodedToken
+            next()
+        }
+    })
+  } else {
+      res.status(401).json({message: 'Please provide credentials'})
+  }
+} 
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const server = require('./api/server.js');
 
 const PORT = process.env.PORT || 3300;
+server.get('/', (req, res) => {
+  res.status(200).json({api : "up"})
+})
+
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
 });
